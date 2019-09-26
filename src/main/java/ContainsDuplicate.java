4f9d6d7c8d779cc720f1fd9ba2c15437dd03dc85
Alex Smaliy <alex.smaliy@gmail.com>
2019-09-26 01:42:42 -0700

    ContainsDuplicate

---
+++ src/main/java/ContainsDuplicate.java
@@ -0,0 +1,19 @@
+package lc;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+    static class Solution {
+        public boolean containsDuplicate(int[] nums) {
+            Arrays.sort(nums);
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] == nums[i-1]) return true;
+            }
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().containsDuplicate(new int[] {1, 2, 3, 4, 5}));
+    }
+}
