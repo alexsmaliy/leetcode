cac38d7c7abf7b3e4fd5b18cbebfa2d1e5cedd90
Alex Smaliy <alex.smaliy@gmail.com>
2019-09-29 16:53:16 -0700

    polish "reverse linked list"

--- src/main/java/Leet206ReverseLinkedList.java
+++ src/main/java/Leet206ReverseLinkedList.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class ReverseLinkedList {
-    // Definition for singly-linked list.
+public class Leet206ReverseLinkedList {
+
+    // LeetCode's definition for singly-linked list,
+    // with slight usability modifications.
     public static class ListNode {
         int val;
         ListNode next;
         ListNode(int x) { val = x; }
+
         @Override
         public String toString() {
             List<String> x = new ArrayList<>();
@@ -30,18 +33,26 @@ public class ReverseLinkedList {
     }
 
     static class Solution {
+        // The iterative process to reverse a singly linked list.
+        // This is written as an in-place reversal, but doesn't
+        // have to be. We iterate down the list with an accumulator.
+        // At each step, we append the accumulator to the current
+        // node, then set the accumulator to the result of that.
         public ListNode reverseList(ListNode head) {
-            ListNode toCopy = head;
             ListNode acc = null;
-            while (toCopy != null) {
-                ListNode copy = toCopy;
-                toCopy = toCopy.next;
+            while (head != null) {
+                ListNode copy = head;
+                head = head.next;
                 copy.next = acc;
                 acc = copy;
             }
             return acc;
         }
 
+        // The non-tail-recursive way to reverse a linked list.
+        // We recursively reverse the tail of the list, then
+        // append the head to the end of the result. The repeated
+        // traversal is wasteful.
         public ListNode reverseListRec(ListNode head) {
             if (head.next == null) {
                 return head;
@@ -53,6 +64,14 @@ public class ReverseLinkedList {
             return rest;
         }
 
+        // The tail-recursive way to reverse a linked list.
+        // Start with an empty accumulator. At each iteration,
+        // take the current head node of the input list and
+        // append the accumulator to it, then set the
+        // accumulator to the result of that operation.
+        // Finally, recurse into the rest of the list until
+        // the end is reached. This is not an in-place reversal,
+        // but certainly can be rewritten as one.
         public ListNode reverseListTailRec(ListNode head) {
             return reverseListTailRec(head, null);
         }
