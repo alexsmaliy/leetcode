8b7122f3b1da2d490d6ffabb58c536dc9ba18ea6
Alex Smaliy <alex.smaliy@gmail.com>
2019-10-28 01:31:53 -0700

    clean up leet 139

--- src/main/java/leet/problems/leet139/Leet139WordBreak.java
+++ src/main/java/leet/problems/leet139/Leet139WordBreak.java
@@ -1,5 +1,6 @@
 package leet.problems.leet139;
 
+import java.util.BitSet;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -8,18 +9,20 @@ public class Leet139WordBreak {
     public static boolean wordBreak(String s, List<String> wordDict) {
         Set<String> set = new HashSet<>();
         int maxLength = 0;
+        BitSet lengths = new BitSet();
         for (String word : wordDict) {
             maxLength = Math.max(maxLength, word.length());
             set.add(word);
+            lengths.set(word.length());
         }
         boolean[] tracker = new boolean[s.length()];
-        for (int i = 0; i < Math.min(s.length(), maxLength); i++) {
-            tracker[i] = set.contains(s.substring(0, i + 1));
+        for (int i = lengths.nextSetBit(0); i > -1 && i <= s.length(); i = lengths.nextSetBit(i + 1)) {
+            tracker[i - 1] = set.contains(s.substring(0, i));
         }
         for (int i = 1; i < s.length(); i++) {
-            for (int j = i; j < Math.min(i + maxLength, s.length()); j++) {
-                tracker[j] = tracker[j]
-                    || (tracker[i - 1] && set.contains(s.substring(i, j + 1)));
+            for (int j = lengths.nextSetBit(0); j > -1 && j + i <= s.length(); j = lengths.nextSetBit(j + 1)) {
+                tracker[i + j - 1] = tracker[i + j - 1]
+                    || (tracker[i - 1] && set.contains(s.substring(i, i + j)));
             }
         }
         return tracker[s.length() - 1];
